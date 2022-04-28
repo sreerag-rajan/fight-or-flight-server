5224b5cb93891a7210f0a50ee8c1f7116dd05f53
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-28 19:22:59 +0530

    applied basic search functionality for flights between two airports

--- src/controllers/flights.controller.js
+++ src/controllers/flights.controller.js
@@ -21,5 +21,26 @@ router.get("/allflights", async (req,res)=>{
         return res.status(500).send(er.message);
     }
 })
+router.get("/search", async (req,res)=>{
+    try{
+        console.log(req.query)
+        let flights;
+        if(req.query.start===""&&req.query.end===""){
+            flights = await Flight.find().populate("startAirport").populate("endAirport").lean().exec()
+        }
+        else{
+            flights = await Flight.find({$and:[{startAirport:req.query.start}, {endAirport:req.query.end}]}).populate("startAirport").populate("endAirport").lean().exec();
+        }
+        // let x = flights.filter((el)=>{
+        //     if(el.startAirport===req.params.start&&el.endAirport===req.params.end){
+        //         return el;
+        //     }
+        // })
+        return res.status(200).send(flights);
+    }
+    catch(er){
+        return res.status(500).send(er.message)
+    }
+})
 
 module.exports = router;
